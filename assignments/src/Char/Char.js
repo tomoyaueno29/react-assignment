62bfc00b10f05557be57cb181c583ae2782a42bf
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-21 13:29:27 +0900

    Create Char.js

---
+++ assignments/src/Char/Char.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const char = () => {
+  const style = {
+    display: 'inline-block',
+    padding: '16px',
+    margin: '16px',
+    border: '1px solid black',
+    textalign: 'center'
+  };
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default char;
